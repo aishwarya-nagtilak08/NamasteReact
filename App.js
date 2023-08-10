f6ee095a45ee0737c595f9e3a725aa7f5b0f3949
Aishwarya Nagtilak <aishwarya@Aishwaryas-MacBook-Air.local>
2023-08-10 22:02:50 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+// createElement take input tag, object, content placed in object
+// React Element is nothing but a react object that browsers can understand
+// render convert react object into HTML
+
+const parent = React.createElement("div", {id : "parent"}, [
+    React.createElement("div", {id : "child"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+    ]),
+    React.createElement("div", {id : "child2"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
