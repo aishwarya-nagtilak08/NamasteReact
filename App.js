357c2709a5a59e8a7e35dadb4289661519835a16
Aishwarya Nagtilak <aishwarya@Aishwaryas-MacBook-Air.local>
2023-08-17 23:05:28 +0530

    single rest card added

--- App.js
+++ App.js
@@ -22,10 +22,40 @@ const Header = () => {
   );
 };
 
+const RestoaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        // src={
+        //   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+        //   cloudinaryImageId
+        // }
+        src="https://www.designevo.com/res/templates/thumb_small/separated-brown-burger.webp"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestoaurantCard />
+      </div>
+    </div>
+  );
+};
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
